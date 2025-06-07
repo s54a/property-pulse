692ca8fc4a38d09f001995b3f9867d0a512862d3
Sooraj Gupta <soorajgupta00@gmail.com>
2025-06-07 17:56:36 +0530

    dbConnect

---
+++ src/config/database.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+let connected = false;
+
+const connectDB = async () => {
+	mongoose.set("strictQuery", true);
+
+	if (connected) {
+		console.log("MongoDB is already connected...");
+		return;
+	}
+
+	// Connect to MongoDB
+	try {
+		const uri = process.env.MONGODB_URI;
+
+		if (!uri) {
+			throw new Error("MONGODB_URI is not defined in environment variables");
+		}
+
+		await mongoose.connect(uri);
+		connected = true;
+		console.log("MongoDB Connected...");
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export default connectDB;
