02aa793614b68f9d0c2513b4af5931e70f6018bf
Sooraj Gupta <soorajgupta00@gmail.com>
2025-06-07 20:31:48 +0530

    created Models

---
+++ models/User.ts
@@ -0,0 +1,34 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema(
+	{
+		email: {
+			type: String,
+			unique: [true, "Email Already Exists"],
+			required: [true, "Email is Required"],
+		},
+
+		username: {
+			type: String,
+			required: [true, "Username is Required"],
+		},
+
+		image: {
+			type: String,
+		},
+
+		bookmarks: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "Property",
+			},
+		],
+	},
+	{
+		timestamps: true,
+	},
+);
+
+const User = models.user || model("User", UserSchema);
+
+export default User;
