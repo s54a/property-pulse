1c25ae8954e0ec8c02c7378ff3c7c3c373e62548
Sooraj Gupta <soorajgupta00@gmail.com>
2025-06-10 20:41:34 +0530

    not working code

--- src/app/api/properties/route.ts
+++ src/app/api/properties/route.ts
@@ -1,10 +1,13 @@
 import connectDB from "@/config/database";
+import Property from "@/models/Property";
 
 export const GET = async () => {
 	try {
 		await connectDB();
 
-		return new Response(JSON.stringify({ message: "hello world" }), { status: 200 });
+		const properties = await Property.find({});
+
+		return new Response(JSON.stringify(properties), { status: 200 });
 	} catch (error) {
 		console.log(error);
 		return new Response("Something Went Wrong", {
