07dfa1daba53f9f55e0b06bd4e1fbea99dbd7a87
Sooraj Gupta <soorajgupta00@gmail.com>
2025-06-13 20:08:34 +0530

    property page created & dynamic now

--- src/app/properties/[id]/page.tsx
+++ src/app/properties/[id]/page.tsx
@@ -3,20 +3,11 @@
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { fetchProperty } from "@/utils/requests";
-import PropertyImageHeader from "@/components/PropertyImageHeader";
 import { Property } from "@/types";
 import Link from "next/link";
-import {
-	FaArrowLeft,
-	FaCheck,
-	FaBookmark,
-	FaShare,
-	FaBed,
-	FaBath,
-	FaRulerCombined,
-	FaMoneyBill,
-	FaMapMarker,
-} from "react-icons/fa";
+import PropertyImageHeader from "@/components/PropertyImageHeader";
+import PropertyDetails from "@/components/PropertyDetails";
+import { FaArrowLeft } from "react-icons/fa";
 
 const PropertyPage = () => {
 	const { id } = useParams();
@@ -74,114 +65,7 @@ const PropertyPage = () => {
 					<section className="bg-blue-50">
 						<div className="container m-auto px-6 py-10">
 							<div className="grid w-full grid-cols-1 gap-6 md:grid-cols-[70%_28%]">
-								<main>
-									<div className="rounded-lg bg-white p-6 text-center shadow-md md:text-left">
-										<div className="mb-4 text-gray-500">Apartment</div>
-										<h1 className="mb-4 text-3xl font-bold">Boston Commons Retreat</h1>
-										<div className="mb-4 flex justify-center align-middle text-gray-500 md:justify-start">
-											<i className="fa-solid fa-location-dot mr-2 text-lg text-orange-700"></i>
-											<p className="text-orange-700">120 Tremont Street Boston, MA 02111</p>
-										</div>
-
-										<h3 className="my-6 bg-gray-800 p-2 text-lg font-bold text-white">
-											Rates & Options
-										</h3>
-										<div className="flex flex-col justify-around md:flex-row">
-											<div className="mb-4 flex items-center justify-center border-b border-gray-200 pb-4 md:border-b-0 md:pb-0">
-												<div className="mr-2 font-bold text-gray-500">Nightly</div>
-												<div className="text-2xl font-bold">
-													<i className="fa fa-xmark text-red-700"></i>
-												</div>
-											</div>
-											<div className="mb-4 flex items-center justify-center border-b border-gray-200 pb-4 md:border-b-0 md:pb-0">
-												<div className="mr-2 font-bold text-gray-500">Weekly</div>
-												<div className="text-2xl font-bold text-blue-500">$1,100</div>
-											</div>
-											<div className="mb-4 flex items-center justify-center pb-4 md:pb-0">
-												<div className="mr-2 font-bold text-gray-500">Monthly</div>
-												<div className="text-2xl font-bold text-blue-500">$4,200</div>
-											</div>
-										</div>
-									</div>
-
-									<div className="mt-6 rounded-lg bg-white p-6 shadow-md">
-										<h3 className="mb-6 text-lg font-bold">Description & Details</h3>
-										<div className="mb-4 flex justify-center gap-4 space-x-9 text-xl text-blue-500">
-											<p>
-												<i className="fa-solid fa-bed"></i> 3
-												<span className="hidden sm:inline">Beds</span>
-											</p>
-											<p>
-												<i className="fa-solid fa-bath"></i> 2
-												<span className="hidden sm:inline">Baths</span>
-											</p>
-											<p>
-												<i className="fa-solid fa-ruler-combined"></i>
-												1,500 <span className="hidden sm:inline">sqft</span>
-											</p>
-										</div>
-										<p className="mb-4 text-gray-500">
-											This is a beautiful apartment located near the commons
-										</p>
-										<p className="mb-4 text-gray-500">
-											We have a beautiful apartment located near the commons. It is a 2 bedroom
-											apartment with a full kitchen and bathroom. It is available for weekly or
-											monthly rentals.
-										</p>
-									</div>
-
-									<div className="mt-6 rounded-lg bg-white p-6 shadow-md">
-										<h3 className="mb-6 text-lg font-bold">Amenities</h3>
-
-										<ul className="grid list-none grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-											<li>
-												<FaCheck className="mt-3 mr-2 text-green-600" /> Wifi
-											</li>
-											<li>
-												<FaCheck className="mt-3 mr-2 text-green-600" /> Full kitchen
-											</li>
-											<li>
-												<FaCheck className="mt-3 mr-2 text-green-600" /> Washer & Dryer
-											</li>
-											<li>
-												<FaCheck className="mt-3 mr-2 text-green-600" /> Free Parking
-											</li>
-											<li>
-												<FaCheck className="mt-3 mr-2 text-green-600" /> Hot Tub
-											</li>
-											<li>
-												<FaCheck className="mt-3 mr-2 text-green-600" /> 24/7 Security
-											</li>
-											<li>
-												<FaCheck className="mt-3 mr-2 text-green-600" /> Wheelchair Accessible
-											</li>
-											<li>
-												<FaCheck className="mt-3 mr-2 text-green-600" /> Elevator Access
-											</li>
-											<li>
-												<FaCheck className="mt-3 mr-2 text-green-600" /> Dishwasher
-											</li>
-											<li>
-												<FaCheck className="mt-3 mr-2 text-green-600" /> Gym/Fitness Center
-											</li>
-											<li>
-												<FaCheck className="mt-3 mr-2 text-green-600" /> Air Conditioning
-											</li>
-											<li>
-												<FaCheck className="mt-3 mr-2 text-green-600" /> Balcony/Patio
-											</li>
-											<li>
-												<FaCheck className="mt-3 mr-2 text-green-600" /> Smart TV
-											</li>
-											<li>
-												<FaCheck className="mt-3 mr-2 text-green-600" /> Coffee Maker
-											</li>
-										</ul>
-									</div>
-									<div className="mt-6 rounded-lg bg-white p-6 shadow-md">
-										<div id="map"></div>
-									</div>
-								</main>
+								<PropertyDetails property={property} />
 
 								{/* <!-- Sidebar --> */}
 								<aside className="space-y-4">
