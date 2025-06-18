ba6180a6a1bfae168b56f984ea2d6ef93fa65341
Sooraj Gupta <soorajgupta00@gmail.com>
2025-06-18 21:09:57 +0530

    boilerplate for form

--- src/app/properties/add/page.tsx
+++ src/app/properties/add/page.tsx
@@ -1,5 +1,15 @@
+import PropertyAddForm from "@/components/PropertyAddForm";
+
 const AddPropertyPage = () => {
-  return <div>AddPropertyPage</div>;
+	return (
+		<section className="bg-blue-50">
+			<div className="container m-auto max-w-2xl py-24">
+				<div className="m-4 mb-4 rounded-md border border-gray-300 bg-white px-6 py-8 shadow-md focus:outline-gray-400 md:m-0">
+					<PropertyAddForm />
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default AddPropertyPage;
